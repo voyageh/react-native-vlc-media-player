b42a422b0511ec1f6e95c98d6490acb3cb1c432c
llixing <249568024@qq.com>
2025-04-12 22:55:30 +0800

    更新MobileVLCKit和TVVLCKit版本至4.0.0a10，优化与Expo的集成配置。

--- expo/ios/withMobileVlcKit.js
+++ expo/ios/withMobileVlcKit.js
@@ -4,32 +4,35 @@ const path = require("path");
 const fs = require("fs");
 
 const withMobileVlcKit = (config, options) => {
-    // No need if you are running RN 0.61 and up
-    if (!options?.ios?.includeVLCKit) {
-        return config;
-    }
+  // No need if you are running RN 0.61 and up
+  if (!options?.ios?.includeVLCKit) {
+    return config;
+  }
 
-    return withDangerousMod(config, [
-        "ios",
-        (config) => {
-            const filePath = path.join(config.modRequest.platformProjectRoot, "Podfile");
+  return withDangerousMod(config, [
+    "ios",
+    (config) => {
+      const filePath = path.join(
+        config.modRequest.platformProjectRoot,
+        "Podfile"
+      );
 
-            const contents = fs.readFileSync(filePath, "utf-8");
+      const contents = fs.readFileSync(filePath, "utf-8");
 
-            const newCode = generateCode.mergeContents({
-                tag: "withVlcMediaPlayer",
-                src: contents,
-                newSrc: "  pod 'MobileVLCKit', '3.6.0'",
-                anchor: /use\_expo\_modules\!/i,
-                offset: 3,
-                comment: "  #",
-            });
+      const newCode = generateCode.mergeContents({
+        tag: "withVlcMediaPlayer",
+        src: contents,
+        newSrc: "  pod 'MobileVLCKit', '4.0.0a10'",
+        anchor: /use\_expo\_modules\!/i,
+        offset: 3,
+        comment: "  #",
+      });
 
-            fs.writeFileSync(filePath, newCode.contents);
+      fs.writeFileSync(filePath, newCode.contents);
 
-            return config;
-        },
-    ]);
+      return config;
+    },
+  ]);
 };
 
 module.exports = withMobileVlcKit;
--- react-native-vlc-media-player.podspec
+++ react-native-vlc-media-player.podspec
@@ -12,6 +12,6 @@ Pod::Spec.new do |s|
   s.tvos.deployment_target = "10.2"
   s.static_framework = true
   s.dependency 'React'
-  s.ios.dependency 'MobileVLCKit', '3.6.0'
-  s.tvos.dependency 'TVVLCKit', '3.6.0'
+  s.ios.dependency 'MobileVLCKit', '4.0.0a10'
+  s.tvos.dependency 'TVVLCKit', '4.0.0a10'
 end
